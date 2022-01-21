be3591a0c0858803339ec6cdc11a9992ce8b21f2
badovsky <haxnoqwe0803>
2022-01-21 19:58:55 +0100

    add deposit

--- src/main/java/datamodel/Deposit.java
+++ src/main/java/datamodel/Deposit.java
@@ -1,5 +1,38 @@
 package datamodel;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Deposit {
+
+    private float percent;
+    private Long accountId;
+    private Long amount;
+    private int months;
+
+    public float getPercent() {
+        return percent;
+    }
+    public void setPercent(float percent) {
+        this.percent = percent;
+    }
+
+    public long getAccountId() {
+        return accountId;
+    }
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public long getAmount() { return amount; }
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public int getMonts() {return months;}
+    public void setMonts(int months) {
+        this.months = months;
+    }
+
 
 }
